5d36d9c64f7660307392f5c41bde3e1f8cbecaef
alin <alin.voicau@nagarro.com>
2024-02-27 11:32:31 +0200

    FB anii drumetiei  added event links from page

--- tests/fb_anii_drumetiei_events.test.ts
+++ tests/fb_anii_drumetiei_events.test.ts
@@ -3,10 +3,17 @@ import fs from "fs";
 
 test("get the events content", async ({ page }) => {
   await page.goto("https://www.facebook.com/anii.drumetiei/events/");
+  await page.getByRole("button", { name: "Decline optional cookies" }).click();
+  await page.getByLabel("Close").click();
 
   await expect(
     page.locator("//div[@class='x1yztbdb']/following-sibling::div[1]")
   ).toBeVisible();
+
+  // Find the locator containing events and their children
+  const eventsLocator = page.locator(
+    "//div[@class='x1yztbdb']/following-sibling::div[1]"
+  );
 
   const eventTextContent1 = await page
     .locator("//div[@class='x1yztbdb']/following-sibling::div[1]")
@@ -17,9 +24,22 @@ test("get the events content", async ({ page }) => {
     .split("Event by Anii Drumeției")
     .map((event) => event.trim())
     .filter((event) => event !== "");
+  // Find all links in the event elements
+  async function getEventLinks(page) {
+    const hrefs = await page.evaluate(() => {
+      const links = Array.from(
+        document.querySelectorAll('a[href*="/events/"]') // Include links with "/events"
+      );
+
+      return links.map((link) => link.href as string);
+    });
+    return hrefs;
+  }
+
+  const eventLinks = await getEventLinks(page);
 
   // Save event content to a JSON file
-  const data = { events: eventsArray };
+  const data = { events: eventsArray, eventLinks };
   const jsonData = JSON.stringify(data, null, 2);
 
   fs.writeFileSync("events.json", jsonData);
